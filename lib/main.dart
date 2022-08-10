3979e6ae7bd652ee99f0d3b8a4209764ca387bd7
Shuvo <sohorafuzzamanshuvo@gmail.com>
2022-08-10 13:46:05 +0600

    grid view skleton

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_app/Pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+
